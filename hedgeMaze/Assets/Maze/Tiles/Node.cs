bcd6336adcd7f0100a1fabae78b2205c357d9e3f
jill <jillian.chapin@mymail.champlain.edu>
2026-03-28 16:41:54 -0400

    Start of Hedge Setup
    Find Way to expose NodeTypes

--- hedgeMaze/Assets/Maze/Tiles/Node.cs
+++ hedgeMaze/Assets/Maze/Tiles/Node.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
 
+public enum ENodetype
+{
+    HEDGE,
+    NO_HEDGE,
+    NUM_TYPES
+}
 public class Node 
 {
     [SerializeField] Vector2Int coords;
+    [SerializeField] ENodetype type;
     public Node(Vector2Int coords)
     {
         this.coords = coords;
