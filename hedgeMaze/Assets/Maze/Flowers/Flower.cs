f37959dddc08fae666b5d693ba5fa42f778a5081
jill <jillian.chapin@mymail.champlain.edu>
2026-04-22 10:19:49 -0400

    Watering Feedback & Bugfix
    Watering flowers increases their height
    You cannot increase the water amount of flowers that are fully watered

--- hedgeMaze/Assets/Maze/Flowers/Flower.cs
+++ hedgeMaze/Assets/Maze/Flowers/Flower.cs
@@ -5,6 +5,7 @@ public class Flower : MonoBehaviour
     public bool isWatered;
     [SerializeField] float waterValue;
     [SerializeField] float waterRequirement;
+    [SerializeField] float waterHeightIncrease;
     [Header("References")]
     [SerializeField] MeshRenderer meshRenderer;
     [SerializeField] Material waterMaterial;
@@ -23,7 +24,10 @@ public class Flower : MonoBehaviour
 
     public void Water(float waterAmount)
     {
+        if (isWatered)
+            return;
         waterValue += waterAmount * Time.deltaTime;
+        transform.position = new Vector3(transform.position.x, transform.position.y + (waterHeightIncrease * Time.deltaTime), transform.position.z);
         if (waterValue > waterRequirement)
         {
             isWatered = true;
