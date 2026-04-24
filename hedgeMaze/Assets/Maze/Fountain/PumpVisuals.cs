3240a4ad82265263362161dd8d1b0bfb87a22dbb
jill <jillian.chapin@mymail.champlain.edu>
2026-04-24 15:37:21 -0400

    Pump Fix

--- hedgeMaze/Assets/Maze/Fountain/PumpVisuals.cs
+++ hedgeMaze/Assets/Maze/Fountain/PumpVisuals.cs
@@ -8,7 +8,6 @@ public class PumpVisuals : MonoBehaviour
     [SerializeField] float startAngle;
     [SerializeField] Transform handle;
     [SerializeField] Transform pusher;
-    [SerializeField] Transform pusherTarget;
     [Range(0f, 1f)]
     [SerializeField] public float amount;
     void Start()
@@ -20,7 +19,7 @@ public class PumpVisuals : MonoBehaviour
     void Update()
     {
         float angle = Mathf.Lerp(minAngle, maxAngle, amount);
-        handle.transform.localEulerAngles = new Vector3(handle.transform.eulerAngles.x, handle.transform.eulerAngles.y, angle);
-        pusher.LookAt(pusherTarget, Vector3.right);
+        handle.transform.localEulerAngles = new Vector3(handle.transform.localEulerAngles.x, handle.transform.localEulerAngles.y, angle);
+        pusher.eulerAngles = Vector3.zero;
     }
 }
